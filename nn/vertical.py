8f2866e63aece857659d8875f3ca0ade5d948bea
debnsuma <debnsuma@amazon.com>
2024-07-10 22:42:01 -0400

    adding the nnfs code ch3

--- nn/vertical.py
+++ nn/vertical.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# Modified from:
 # Copyright (c) 2015 Andrej Karpathy
 # License: https://github.com/cs231n/cs231n.github.io/blob/master/LICENSE
 # Source: https://cs231n.github.io/neural-networks-case-study/
@@ -9,8 +10,6 @@ def create_data(samples, classes):
     y = np.zeros(samples*classes, dtype='uint8')
     for class_number in range(classes):
         ix = range(samples*class_number, samples*(class_number+1))
-        r = np.linspace(0.0, 1, samples)
-        t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        X[ix] = np.c_[np.random.randn(samples)*.1 + (class_number)/3, np.random.randn(samples)*.1 + 0.5]
         y[ix] = class_number
     return X, y
